7f5213e872d06177e812e77426e74ebe94e02bd8
Carlo <carlodri@users.noreply.github.com>
2018-10-10 19:11:29 +0200

    add back flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
 if [ $(uname) == Linux ]; then
-  export CXXFLAGS="${CXXFLAGS/" -std=c++17"/} -std=c++11"
+  export CXXFLAGS="${CXXFLAGS/" -std=c++17"/} -std=c++11 -I${PREFIX}/include/boost"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 fi
+
+echo $(uname)
+echo $CXXFLAGS
 
 mkdir build
 cd build
