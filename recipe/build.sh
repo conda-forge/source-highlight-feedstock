a1cf95a18dd989b5f3700be66c867cf474838068
Carlo <carlodri@users.noreply.github.com>
2018-10-10 10:47:49 +0200

    add flags in linux

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
 if [ $(uname) == Linux ]; then
-  export CXXFLAGS=${CXXFLAGS/" -std=c++17"/}
+  export CXXFLAGS="${CXXFLAGS/" -std=c++17"/} -I${PREFIX}/include/boost"
+  export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
 fi
 
 echo $(uname)
 echo $CXXFLAGS
+echo $LDFLAGS
 
 mkdir build
 cd build
