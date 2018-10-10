8e44a6e1d1755f95389c127fa177126cafac52db
Isuru Fernando <isuruf@gmail.com>
2018-10-10 16:06:10 -0500

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,7 @@
 
 if [ $(uname) == Linux ]; then
   export CXXFLAGS="${CXXFLAGS/" -std=c++17"/}"
+  export LDFLAGS="$LDFLAGS -Wl,-rpath-link,$PREFIX/lib"
   # TODO: remove this hack and fix the build system issue with gcc7 compiler flags
   export LD_LIBRARY_PATH="$PREFIX/lib:$LD_LIBRARY_PATH"
 fi
