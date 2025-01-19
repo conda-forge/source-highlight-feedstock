ff344af22fe4b6b5207b28ac23da337f8f08eef4
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 21:28:42 -0800

    use C++11

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,7 +2,7 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
-export CXXFLAGS="${CXXFLAGS} -Wno-register -Wno-dynamic-exception-spec"
+export CXXFLAGS="${CXXFLAGS} -std=c++11"
 autoreconf --force --verbose --install
 ./configure \
     --prefix=$PREFIX \
