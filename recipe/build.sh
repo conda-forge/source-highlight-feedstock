5621af71ae6f09b859ced035eeda2e3c4b2eb8e8
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-18 21:59:38 -0800

    fix cross-compile

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,6 +2,34 @@
 
 set -o xtrace -o nounset -o pipefail -o errexit
 
+if [[ ${CONDA_BUILD_CROSS_COMPILATION:-0} == 1 ]]; then
+    CROSS_LDFLAGS=${LDFLAGS}
+    CROSS_CC="${CC}"
+    CROSS_CXX="${CXX}"
+    CROSS_LD="${LD}"
+
+    LDFLAGS=${LDFLAGS//${PREFIX}/${BUILD_PREFIX}}
+    CC=${CC//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}
+    CXX=${CXX//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}
+    LD="${LD//${CONDA_TOOLCHAIN_HOST}/${CONDA_TOOLCHAIN_BUILD}}"
+
+    export CXXFLAGS="${CXXFLAGS} -std=c++11"
+    autoreconf --force --verbose --install
+    ./configure \
+        --prefix=$BUILD_PREFIX \
+        --with-boost-libdir=${BUILD_PREFIX}/lib
+    make -j${CPU_COUNT}
+    make install
+    make clean
+
+    LDFLAGS="${CROSS_LDFLAGS}"
+    CC=${CROSS_CC}
+    CXX=${CROSS_CXX}
+    LD=${CROSS_LD}
+
+    sed -i "s?\$(top_builddir)/src?${BUILD_PREFIX}/bin?" doc/Makefile.am
+fi
+
 export CXXFLAGS="${CXXFLAGS} -std=c++11"
 autoreconf --force --verbose --install
 ./configure \
